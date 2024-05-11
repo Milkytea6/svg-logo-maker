99be2edec4d0ad10a80e7609919cb8b99c801201
Christopher Giordano <chrisgcpg@gmail.com>
2024-05-11 17:46:20 -0400

    Added a function to run the constructor classes for each shape. Also have render functions specific to each shape that can be made.

--- index.js
+++ index.js
@@ -1,28 +1,73 @@
+// required imports for this js file
+const fs = require('fs');
 const inquirer = require('inquirer');
+const colors = require('colors');
+const { Square, Circle, Triangle } = require('./lib/shapes');
 
 // an array of prompt for the logo
 const logoPrompts = [
     {
         type: 'input',
         name: 'characters',
-        message: 'Enter three characters that you would inside your logo.'
+        message: (colors.brightBlue('Enter three characters that you would inside your logo.'))
     },
     {
         type: 'input',
         name: 'textColor',
-        message: 'Enter the color of the characters inside your logo.'
+        message: (colors.brightMagenta('Enter the color of the characters inside your logo.'))
     },
     {
-        type: 'input',
+        type: 'list',
         name: 'shape',
-        message: 'Enter the shape of your logo'
+        message: (colors.brightGreen('Enter the shape of your logo')),
+        choices: ['Square', 'Circle', 'Triangle']
     },
     {
         type: 'input',
         name: 'shapeColor',
-        message: 'Enter the color of the shape'
+        message: (colors.brightRed('Enter the color of the shape'))
     }
 ];
+
+inquirer
+    .prompt(logoPrompts)
+
+    .then((response) => {
+        // Switch statment to choose the correct class to run the render().
+        switch (response.shape) {
+            case 'Square':
+                logoClass = new Square(response.characters, response.textColor, response.shape, response.shapeColor);
+                break;
+            case 'Circle':
+                logoClass = new Circle(response.characters, response.textColor, response.shape, response.shapeColor);
+                break;
+            case 'Triangle':
+                logoClass = new Triangle(response.characters, response.textColor, response.shape, response.shapeColor);
+                break;
+            default:
+                console.log('Invaild Shape');
+        }
+        console.log(logoClass);
+
+        const svgCode = logoClass.render(response);
+        if (svgCode) {
+            console.log(svgCode);
+            // appends new svg file to the folder
+            fs.writeFile('generated-logo.svg', svgCode, (err) =>
+                err ? console.log(err) : console.log('Successfully created svg file')
+            );
+        }
+        else {
+            console.log("Error: svgCode is returning falsy");
+        }
+    });
+
+
+
+
+
+
+
 
 
 
